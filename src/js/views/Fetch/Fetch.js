4aaeb6c15045571eb0e51c553ae04b2a43e8f8d2
samir benzada <samirbenzada@gmail.com>
2020-02-01 11:26:12 -0500

    -m

--- src/js/views/Fetch/Fetch.js
+++ src/js/views/Fetch/Fetch.js
@@ -2,12 +2,20 @@ import React from 'react';
 import styles from './Fetch.module.css';
 
 const Fetch = () => {
+
+    const fetchHandler = () => {
+        console.log('helooo');
+    }
+
     return (
-        <div className={styles.blackBg}>
+        <div className={styles.mainDiv}>
             <div className="container">
                 <div className="row">
                     <div className="col text-center">
-                        <button className="btn btn-warning" >Click To Fecth</button>
+                        <button 
+                            className="btn btn-warning" 
+                            onClick={fetchHandler}
+                        >Click To Fecth</button>
                     </div>
                 </div>
             </div>
